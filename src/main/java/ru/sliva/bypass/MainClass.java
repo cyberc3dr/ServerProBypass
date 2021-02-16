219d3c4f1a72b2626f16f8f6952c4ba0593febac
Geo3gamer <geo3gamer@gmail.com>
2021-02-16 13:10:54 +0300

    Безлимитные слоты, перенаправление команды.

--- src/main/java/ru/sliva/bypass/MainClass.java
+++ src/main/java/ru/sliva/bypass/MainClass.java
@@ -8,26 +8,47 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerLoginEvent.Result;
+import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MainClass extends JavaPlugin {
+public class MainClass extends JavaPlugin implements Listener{
 	
 	private Plugin[] plugins;
 	private PluginManager pManage;
 	private Logger logger;
 	private File pluginsFolder;
+	private FileConfiguration config;
 	
 	@Override
 	public void onEnable() {
 		logger = getLogger();
+		
 		pManage = Bukkit.getPluginManager();
+		pManage.registerEvents(this, this);
+		
+		File configFile = new File(getDataFolder() + File.separator + "config.yml");
+		if(!configFile.exists()) {
+			getLogger().info("Creating a new configuration...");
+			getConfig().options().copyDefaults(true);
+			saveDefaultConfig();
+		}
+		
+		config = getConfig();
+		
 		pluginsFolder = new File(getDataFolder() + File.separator + "plugins");
 		if(!pluginsFolder.exists()) {
 			logger.warning("Plugins folder not found! Making a new one...");
@@ -35,6 +56,7 @@ public class MainClass extends JavaPlugin {
 		}
 		plugins = pManage.loadPlugins(pluginsFolder);
 		logger.warning("Plugins enabled successfully!");
+		
 	}
 	
 	@Override
@@ -66,30 +88,47 @@ public class MainClass extends JavaPlugin {
     
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-    	if(command.getName().equalsIgnoreCase("say")) {
-			if(!(sender instanceof ConsoleCommandSender)) {
-				return false;
-			}
-			StringBuilder str = new StringBuilder();
-	        for (int i = 0; i< args.length;i++)
-	        {
-	            str.append(args[i] + " ");
-	        }
-	        String message = str.toString().trim();
-	        if(message.contains("ads")) {
-	        	logger.info("Ad blocked!");
-	        	return true;
-	        } else {
-	        	for(Player p : Bukkit.getOnlinePlayers()) {
-	        		if(p.isOp()) {
-	        			p.sendMessage(message);
-	        			p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING , 1.0f, 100);
-	        		}
-	        	}
-	        	Bukkit.getConsoleSender().sendMessage(message);
-	        }	
-			return true;
+    	if(sender.hasPermission("minecraft.command.say")) {
+        	StringBuilder str = new StringBuilder();
+        	for(int i = 1; i < args.length; i++) {
+        		str.append(args[i]);
+        		if(i < args.length) str.append(" ");
+        	}
+        	String msg = str.toString();
+        	if(sender instanceof ConsoleCommandSender) {
+        		if(msg.contains("ads")) {
+        			sender.sendMessage("Ad blocked!");
+        		} else {
+        			for(Player p : Bukkit.getOnlinePlayers()) {
+        				if(p.isOp()) {
+        					p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 100, 1);
+        					p.sendMessage("[Server] " + msg);
+        				}
+        			}
+        		}
+        		return true;
+        	}
+        	Bukkit.dispatchCommand(sender, "minecraft:say " + msg);
+    	} else {
+    		sender.sendMessage(Bukkit.getPermissionMessage());
     	}
     	return true;
+    }
+    
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onLogin(PlayerLoginEvent e) {
+    	if(config.getBoolean("unlimited-slots")) {
+        	if(e.getResult() == Result.KICK_FULL) {
+        		e.allow();
+        	}
+    	}
+    }
+    
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPing(ServerListPingEvent e) {
+    	if(config.getBoolean("unlimited-slots")) {
+    		e.setMaxPlayers(e.getNumPlayers() + 1);
+    	}
+    	e.setMotd(ChatColor.translateAlternateColorCodes('&', config.getString("motd").replace("/n", "\n")));
     }
 }

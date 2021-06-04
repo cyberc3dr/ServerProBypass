0e49cadae8e800089614e85175b677ebb8215087
Geo3gamer <geo3gamer@gmail.com>
2021-06-04 13:46:14 +0300

    1.3

--- src/main/kotlin/ru/sliva/bypass/MainClass.kt
+++ src/main/kotlin/ru/sliva/bypass/MainClass.kt
@@ -1,54 +1,104 @@
 package ru.sliva.bypass
 
-import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
-import java.lang.StringBuilder
-import org.bukkit.command.ConsoleCommandSender
-import org.bukkit.entity.Player
 import org.bukkit.Sound
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
+import org.bukkit.command.ConsoleCommandSender
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerLoginEvent
 import org.bukkit.event.server.ServerListPingEvent
+import org.bukkit.permissions.Permission
 import org.bukkit.plugin.Plugin
-import org.bukkit.plugin.PluginManager
+import org.bukkit.plugin.PluginLoadOrder
+import org.bukkit.plugin.SimplePluginManager
+import org.bukkit.plugin.java.JavaPlugin
 import java.io.File
-import java.util.logging.Logger
+import java.util.logging.Level
+
 
 class MainClass : JavaPlugin(), Listener {
 
     private lateinit var plugins: Array<Plugin>
-    private var pManage = Bukkit.getPluginManager()
+    private var pluginManager = Bukkit.getPluginManager() as SimplePluginManager
 
-    override fun onEnable() {
+    override fun onLoad() {
         dataFolder.mkdirs()
-        pManage.registerEvents(this, this)
         val configFile = File(dataFolder, "config.yml")
         if (!configFile.exists()) {
             logger.info("Creating a new configuration...")
             config.options().copyDefaults(true)
             saveDefaultConfig()
         }
-        val pluginsFolder = File(dataFolder, "plugins")
-        if (!pluginsFolder.exists()) {
-            logger.warning("Plugins folder not found! Making a new one...")
-            pluginsFolder.mkdirs()
-        }
-        plugins = pManage.loadPlugins(pluginsFolder)
+        loadPlugins()
+        enablePlugins(PluginLoadOrder.STARTUP)
+    }
+
+    override fun onEnable() {
+        pluginManager.registerEvents(this, this)
+        enablePlugins(PluginLoadOrder.POSTWORLD)
         logger.warning("Plugins enabled successfully!")
     }
 
     override fun onDisable() {
-        logger.warning("Disabling plugins!")
-        for (p in plugins) {
-            pManage.disablePlugin(p, true)
-        }
+        disablePlugins()
         logger.info("Plugins disabled successfully!")
+    }
+
+    private fun loadPlugins() {
+        val pluginFolder = File(dataFolder, "plugins")
+        if (!pluginFolder.exists()) {
+            logger.warning("Plugins folder not found! Making a new one...")
+            pluginFolder.mkdirs()
+        }
+        if (pluginFolder.exists()) {
+            val plugins = pluginManager.loadPlugins(pluginFolder)
+            for (plugin in plugins) {
+                try {
+                    val message = String.format("Loading %s", plugin.description.fullName)
+                    plugin.logger.info(message)
+                    plugin.onLoad()
+                } catch (ex: Throwable) {
+                    logger.log(Level.SEVERE, ex.message + " initializing " + plugin.description.fullName + " (Is it up to date?)", ex)
+                }
+            }
+        } else {
+            pluginFolder.mkdir()
+        }
+    }
+
+    private fun enablePlugins(type : PluginLoadOrder) {
+        for (plugin in plugins) {
+            if (!plugin.isEnabled && plugin.description.load == type) {
+                enablePlugin(plugin)
+            }
+        }
+    }
+
+    private fun enablePlugin(plugin: Plugin) {
+        try {
+            val perms: List<Permission> = plugin.description.permissions
+            for (perm in perms) {
+                try {
+                    pluginManager.addPermission(perm, false)
+                } catch (ex: IllegalArgumentException) {
+                    logger.log(Level.WARNING, "Plugin " + plugin.description.fullName + " tried to register permission '" + perm.name + "' but it's already registered", ex)
+                }
+            }
+            pluginManager.dirtyPermissibles()
+            pluginManager.enablePlugin(plugin)
+        } catch (ex: Throwable) {
+            logger.log(Level.SEVERE, ex.message + " loading " + plugin.description.fullName + " (Is it up to date?)", ex)
+        }
+    }
+
+    fun disablePlugins() {
+        for(plugin in plugins) {
+            pluginManager.disablePlugin(plugin, true)
+        }
     }
 
     override fun onTabComplete(sender: CommandSender, command: Command, alias: String, args: Array<String>): List<String> {
